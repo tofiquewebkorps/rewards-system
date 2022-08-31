33659dd166fcf3baf872567d04f03e97c30914af
Tofique Ahmed Khan <tofique@webkorps.com>
2022-08-31 19:01:24 +0530

    Customer Save & Update Test Done

--- src/test/java/com/rewards/CustomerServiceImplTests.java
+++ src/test/java/com/rewards/CustomerServiceImplTests.java
@@ -58,6 +58,7 @@ public class CustomerServiceImplTests {
         log.info("saveCustomerTransaction()-> has been ended");
     }
 
+    @Test
     public void saveCustomerFailerTest() {
         log.info("saveCustomerTransaction()-> has been started");
         customerDto.setName("test1");
@@ -65,11 +66,7 @@ public class CustomerServiceImplTests {
         transactionList.add(new TransactionDTO(120l,0l, LocalDate.of(2022, 01, 02)));
         transactionList.add(new TransactionDTO(125l,0l,LocalDate.of(2022, 01, 03)));
         customerDto.setTransactions(transactionList);
-        CustomerDTO cust = customerService.saveUpdateCustomer(customerDto);
-        Assertions.assertNotNull(cust);
-        Assertions.assertNotNull(cust.getCid());
-        Assertions.assertEquals(cust.getName(),"test1");
-        log.info("saveCustomerTransaction()-> Customer Object :- "+cust.toString());
+        Assertions.assertThrows(NullPointerException.class, ()->customerService.saveUpdateCustomer(null)) ;
         log.info("saveCustomerTransaction()-> has been ended");
     }
 
@@ -78,12 +75,16 @@ public class CustomerServiceImplTests {
     @Test
     public void updatedCustomerTransaction(){
         log.info("updatedCustomerTransaction()-> has been started");
-        customerDto.setName("test1"); customerDto.setTotalRewardPoints(0l);
+        customerDto.setName("test1");
         List<TransactionDTO> transactionList = new ArrayList<TransactionDTO>();
         transactionList.add(new TransactionDTO(120l,0l,LocalDate.of(2022, 01, 02)));
         transactionList.add(new TransactionDTO(125l,0l,LocalDate.of(2022, 01, 03)));
         customerDto.setTransactions(transactionList);
         CustomerDTO cust = customerService.saveUpdateCustomer(customerDto);
+        cust.setName("Test2");
+        CustomerDTO updateCustomer = customerService.saveUpdateCustomer(cust);
+        Assertions.assertEquals(updateCustomer.getName(),"Test2");
+        Assertions.assertNotNull(updateCustomer);
         log.info("saveCustomerTransaction()-> Customer Object :- "+cust.toString());
         log.info("saveCustomerTransaction()-> has been ended");
     }
